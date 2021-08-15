4c734033cb00979b0d6d623ff05f8dc910cf845d
Bogdan Adrian Velica <vbogdan@gmail.com>
2021-08-15 16:06:20 +0000

    Small error fix

--- app/blueprints/site/routes.py
+++ app/blueprints/site/routes.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 blueprint_site = Blueprint('site', __name__)
 
-@site.route('/')
+@blueprint_site.route('/')
 def index():
 	return 'My first index page with blueprint - main page'
--- app/blueprints/users/routes.py
+++ app/blueprints/users/routes.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 blueprint_users = Blueprint('users', __name__, url_prefix='/users')
 
-@main_users.route('/')
+@blueprint_users.route('/')
 def users():
 	return 'Users page - blueprint users'
